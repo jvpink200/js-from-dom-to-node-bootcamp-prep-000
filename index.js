fc53661b209d8d5e6e66f9484ef5c3ff6bec998b
jvpink200 <jvillarr20@gmail.com>
2018-06-27 03:22:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+var name = "Joe"
+var height = "74"
+module.exports = {
+  name,height
+}
+
+if (name === "Susan"){
+  console.log("The name is correct")
+}
+else {
+  console.log("Expected the name to be Susan")
+}
